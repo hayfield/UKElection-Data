a07dc5c66da97dcf8f0914e6dd5130e736a41700
hayfield <hayfield@gmx.com>
2011-05-05 20:50:29 +0100

    Add a crude party detail page

--- src/UKElectionData/election/views.py
+++ src/UKElectionData/election/views.py
@@ -1,7 +1,6 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
 from UKElectionData.election.models import Party, Candidate_2010, Constituency
 
 # display all of the parties
@@ -10,4 +9,5 @@ def partyIndex(request):
     return render_to_response('election/party/index.html', {'parties': parties})
 
 def partyDetail(request, party_id):
-    return HttpResponse("You're looking at party %s." % party_id)
+    party = get_object_or_404(Party, pk=party_id)
+    return render_to_response('election/party/party.html', {'party': party})
