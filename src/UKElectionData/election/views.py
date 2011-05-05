af85e27eb60b2929e84753306c8809f3b5f909e2
hayfield <hayfield@gmx.com>
2011-05-05 20:47:48 +0100

    Render to response shortcut

--- src/UKElectionData/election/views.py
+++ src/UKElectionData/election/views.py
@@ -1,17 +1,13 @@
 # Create your views here.
 
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from UKElectionData.election.models import Party, Candidate_2010, Constituency
 
 # display all of the parties
 def partyIndex(request):
     parties = Party.objects.all().order_by('party_name')
-    t = loader.get_template('election/party/index.html')
-    c = Context({
-        'parties': parties
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('election/party/index.html', {'parties': parties})
 
 def partyDetail(request, party_id):
     return HttpResponse("You're looking at party %s." % party_id)
