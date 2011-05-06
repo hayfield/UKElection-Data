421a54827ae3a0f0dd8b51b20029c66b39be8254
hayfield <hayfield@gmx.com>
2011-05-06 14:35:24 +0100

    Fix the party listing view

--- src/UKElectionData/election/views.py
+++ src/UKElectionData/election/views.py
@@ -5,7 +5,7 @@ from UKElectionData.election.models import Party, Candidate_2010, Constituency_2
 
 # display all of the parties
 def partyIndex(request):
-    parties = Party.objects.all().order_by('party_name')
+    parties = Party.objects.all().order_by('name')
     return render_to_response('election/party/index.html', {'parties': parties})
 
 def partyDetail(request, party_id):
