b7ddc7349338058127da534aa78df5d84b0d23fa
hayfield <hayfield@gmx.com>
2011-05-06 16:24:44 +0100

    Give URLs names. Change file names from 'index' to 'list'.

--- src/UKElectionData/election/urls.py
+++ src/UKElectionData/election/urls.py
@@ -1,42 +1,47 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 from django.views.generic import DetailView, ListView
 from UKElectionData.election.models import Party, Constituency_2010, Candidate_2010
 
-urlpatterns = patterns('election.views',
+urlpatterns = patterns('',
     # party information
     # full party listing
-    (r'^party/$',
+    url(r'^party/$',
         ListView.as_view(
             queryset = Party.objects.order_by('name'),
             context_object_name = 'parties',
-            template_name = 'election/party/index.html')),
+            template_name = 'election/party/list.html'),
+        name = 'party-list'),
                     
     # specific party info   
-    (r'^party/(?P<pk>\d+)/$',
+    url(r'^party/(?P<pk>\d+)/$',
         DetailView.as_view(
             model = Party,
-            template_name = 'election/party/party.html')),
+            template_name = 'election/party/party.html'),
+        name = 'party-detail'),
     
     # constituency information
     # full constituency listing
-    (r'^constituency/$',
+    url(r'^constituency/$',
         ListView.as_view(
             queryset = Constituency_2010.objects.order_by('name'),
             context_object_name = 'constituencies',
-            template_name = 'election/constituency/index.html')),
+            template_name = 'election/constituency/list.html'),
+        name = 'constituency-2010-list'),
     
     # specific constituency info
-    (r'^constituency/(?P<pk>\d+)/$',
+    url(r'^constituency/(?P<pk>\d+)/$',
         DetailView.as_view(
             model = Constituency_2010,
             context_object_name = 'constituency',
-            template_name = 'election/constituency/constituency.html')),
+            template_name = 'election/constituency/constituency.html'),
+        name = 'constituency-2010-detail'),
     
     # candidate 2010 information
     # full candidate listing
-    (r'^candidate/$',
+    url(r'^candidate/$',
         ListView.as_view(
             queryset = Candidate_2010.objects.order_by('name'),
             context_object_name = 'candidates',
-            template_name = 'election/candidate/index.html')),
+            template_name = 'election/candidate/list.html'),
+        name = 'candidate-2010-list'),
 )
