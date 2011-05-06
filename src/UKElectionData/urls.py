7ffe4a93302799631083d09353b95b9fb29b867f
hayfield <hayfield@gmx.com>
2011-05-06 14:45:26 +0100

    Add crude views for candidates, parties and constituencies

--- src/UKElectionData/urls.py
+++ src/UKElectionData/urls.py
@@ -16,6 +16,13 @@ urlpatterns = patterns('',
     (r'^party/$', 'election.views.partyIndex'),
     (r'^party/(?P<party_id>\d+)/$', 'election.views.partyDetail'),
     
+    # constituency information
+    (r'^constituency/$', 'election.views.constituencyIndex'),
+    (r'^constituency/(?P<constituency_id>\d+)/$', 'election.views.constituencyDetail'),
+    
+    # candidate 2010 information
+    (r'^candidate/$', 'election.views.candidate2010Index'),
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
